6be0b176b5f232feda8cda2e15a3c68436bcfdcc
iamejcore <eecore20@students.desu.edu>
2023-10-12 02:35:27 +0000

    started read string

--- lab1.c
+++ lab1.c
@@ -3,3 +3,16 @@
  *
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab1.h"
+
+char* readString(const char* fileName) {
+    FILE* file = fopen(fileName, "r");
+
+    if (file == NULL) {
+        fprintf("File Not Found");
+        return NULL;
+    }
+}
+
