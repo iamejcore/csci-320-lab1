db3d4f0b34cd49eaa9e62e48bb635ad03aec7611
iamejcore <eecore20@students.desu.edu>
2023-10-12 02:37:49 +0000

    third commit

--- lab1.c
+++ lab1.c
@@ -14,5 +14,17 @@ char* readString(const char* fileName) {
         fprintf("File Not Found");
         return NULL;
     }
+    
+    // Find the file size
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    if (file_size > 99) {
+        printf("File size is greater than 99.");
+        fclose(file);
+        return NULL;
+    }
+
 }
 
